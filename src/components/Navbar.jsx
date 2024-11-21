9d2835fad7b82c30fc1be7ac430a4831ebc89721
Ankit <ankit.singh@leverageedu.com>
2024-11-21 18:24:16 +0530

    LinkediN Id of Girman technologies Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,27 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="bg-white shadow-md p-4 flex justify-between">
-      <img
+      <a
+        href="https://girmantech.com" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        <img
           src="https://girmantech.com/Logo2.svg"
           alt="Girman Logo"
           className="navbar__logo-img"
         />
+      </a>
       <div className="flex items-center text-lg space-x-4">
-        <a href="https://girmantech.com" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://girmantech.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Website
         </a>
         <a
-          href="https://linkedin.com/company/girman-technologies"
+          href="https://www.linkedin.com/company/girmantech/"
           target="_blank"
           rel="noopener noreferrer"
         >
